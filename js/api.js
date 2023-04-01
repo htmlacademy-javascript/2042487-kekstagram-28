b4d360d177490411c5970aa97de36b5da98a44ac
AKruhlou <bolyan2@gmail.com>
2023-04-01 10:42:28 +0300

    feractor: api and form modules

--- js/api.js
+++ js/api.js
@@ -1,13 +1,11 @@
 const GET_URL = 'https://28.javascript.pages.academy/kekstagram/data';
 const SEND_URL = 'https://28.javascript.pages.academy/kekstagram/';
 
-const ErrorText = {
-  GET: 'Не удаётся загрузить данные. Проверьте подключение к сети',
-  SEND: 'Не удаётся отправить форму. Проверьте подключение к сети',
-};
+const ERROR_GET = 'Не удаётся загрузить данные. Проверьте подключение к сети';
+const ERROR_SEND = 'Не удаётся отправить форму. Проверьте подключение к сети';
 
 const load = (route, errorText, method, body = null) =>
-  fetch('route', {method, body})
+  fetch(route, {method, body})
     .then((response) => {
       if (!response.ok) {
         throw new Error();
@@ -18,8 +16,8 @@ const load = (route, errorText, method, body = null) =>
       throw new Error(errorText);
     });
 
-const getData = () => load(GET_URL, ErrorText.GET, 'GET');
+const getData = () => load(GET_URL, ERROR_GET, 'GET');
 
-const sendData = (body) => load(SEND_URL, ErrorText.SEND, 'POST', body);
+const sendData = (body) => load(SEND_URL, ERROR_SEND, 'POST', body);
 
 export {getData, sendData};
--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-import {getPhotoWithComments} from './data.js';
+import {getData} from './api.js';
 import {renderGallery} from './gallery.js';
+import {showErrorMeassge} from './messages.js';
+import {setUserFormSubmit} from './form.js';
 import './hidden-favicon.js';
 import './form.js';
 
-renderGallery(getPhotoWithComments());
+getData()
+  .then((usersPictures) => {
+    renderGallery(usersPictures);
+  })
+  .catch((err) => {
+    showErrorMeassge(err.message);
+  });
+
+setUserFormSubmit();
